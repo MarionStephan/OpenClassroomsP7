7b93c99fbfc7c30b0bab44ea1cac6d7a7ca73ee7
MarionStephan <106662880+MarionStephan@users.noreply.github.com>
2023-07-17 11:10:43 +0200

    Correction routes

---
+++ kasa/src/components/Routes/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Apropos from '../../pages/Apropos';
+import Erreur from '../../pages/Erreur';
+import Home from '../../pages/Home';
+import Fiche from '../../pages/Fiche';
+import Header from '../../components/Header';
+
+const IndexRoutes = () => {
+    return(
+        <BrowserRouter>
+        <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route exact path="/home" element={<Home />} />
+                <Route exact path="/apropos" element={<Apropos />} />
+                <Route exact path="/fiche" element={<Fiche />} />
+                <Route path="*" element={<Erreur />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default IndexRoutes;
--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
-import Apropos from './pages/Apropos';
-import Header from './components/Header';
-import Erreur from './components/Erreur';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apropos" element={<Apropos />} />
-        <Route path="*" element={<Erreur />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
