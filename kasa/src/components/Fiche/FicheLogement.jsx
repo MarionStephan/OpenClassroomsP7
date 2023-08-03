823ac0cf08840fb1cf6ae3306ae85b892082c473
MarionStephan <106662880+MarionStephan@users.noreply.github.com>
2023-08-03 15:39:13 +0200

    Responsive pages fiche, erreur, apropos + animations

--- kasa/src/components/Fiche/FicheLogement.jsx
+++ kasa/src/components/Fiche/FicheLogement.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Slideshow from "../../layout/Slideshow/index"
 import Erreur from '../../pages/Erreur'
-import FicheHeader from './FicheHeader'
 import Tag from './Tag'
 import StarRating from './StarRating'
 import Collapse from '../../layout/Collapse/Collapse'
@@ -29,17 +28,22 @@ function FicheLogement() {
         <>
             <Slideshow slides={housing.pictures} />
             <section className='logement'>
-                <FicheHeader key={housing.id}
-                title={housing.title}
-                location={housing.location}
-                host={housing.host.name}
-                picture={housing.host.picture} />
-            
-                <div className='logement__section'>
-                    <p className="logement__section__tags">
-                        {housing.tags.map((tag, index) => (<Tag key={index} tag={tag} />))}
-                    </p>
-                    <StarRating rating={parseInt(housing.rating)} />
+                <div className='logement__header' key={id}>
+                    <div className='logement__infos'>
+                        <h1 className='logement__infos__titre'>{housing.title}</h1>
+                        <p className='logement__infos__loc'>{housing.location}</p>
+                        <p className="logement__infos__tags">
+                            {housing.tags.map((tag, index) => (<Tag key={index} tag={tag} />))}
+                        </p>
+                    </div>
+                    <div className='logement__section'>
+                        <div className='logement__section__host'>
+                            <p className='logement__section__host__para'>{housing.host.name}</p>
+                            <img className='logement__section__host__img' src={housing.host.picture} alt=""></img>
+                        </div>
+                        <StarRating rating={parseInt(housing.rating)} />
+                    </div>
+
                 </div>
                 <div className='logement__collapse'>
                     <Collapse
