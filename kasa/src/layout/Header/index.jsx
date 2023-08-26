ac114f3ae2ff843277c0f46d42f94533383fa1d4
MarionStephan <106662880+MarionStephan@users.noreply.github.com>
2023-08-26 19:07:44 +0200

    Correction css, suppression mixin et animations inutiles

--- kasa/src/layout/Header/index.jsx
+++ kasa/src/layout/Header/index.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.svg'
 function Header() {
     return (
         <nav className='header'>
-            <NavLink to="/"><img src={logo} alt="Kasa Logo" className='header__logo' /></NavLink>
+            <NavLink to="/home"><img src={logo} alt="Kasa Logo" className='header__logo' /></NavLink>
             <ul>
                 <li className='header__nav'><NavLink className={({ isActive}) => (isActive ? "header__nav__link header__nav__link__active" : "header__nav__link header__nav__link__inactive")} to="/">Accueil</NavLink></li>
                 <li className='header__nav'><NavLink className={({ isActive}) => (isActive ? "header__nav__link header__nav__link__active" : "header__nav__link header__nav__link__inactive")} to="/apropos">À propos</NavLink></li>
